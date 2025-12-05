2b5903899d3f921297b426e5c05bb5adf1e94675
Giofani Catalina <ctlnx7@gmail.com>
2025-12-05 16:00:21 +0700

    Update search page dan follow unfollow logic

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { getUser, updateUser, deleteUser } from "../controllers/users.js";
+import { getUser, updateUser, deleteUser, searchUsers } from "../controllers/users.js";
 
 const router = express.Router();
 
 // Jalur untuk mencari user berdasarkan ID (dipakai Frontend di baris 27)
 router.get("/find/:userId", getUser);
+
+// Jalur untuk pencarian user (New Feature)
+router.get("/search", searchUsers);
 
 // Jalur untuk update profile
 router.put("/", updateUser);
---
+++ client-new/.next/dev/static/chunks/UAS_SOSIALMEDIA_client-new_app_page_tsx_c5d6300e._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/196c1_787fb60b._.js"
+],
+    source: "dynamic"
+});
